1165c2f1959e33ddb7f14e09ccb91dcc7cd65a8d
Tony Thompson <penguinprofession@icloud.com>
2018-08-09 11:56:35 -0400

    resolved second part with rest operator and for each method.

--- add/add.ts
+++ add/add.ts
@@ -2,12 +2,18 @@ function add(param1: number, param2: number): number {
     return param1 + param2;
 }
 
-function add2(param1: number[]): number {
-
+function add2(...param1: number[]): number {
+    // created variable to use for total and intialized at 0
+    let total = 0;
+    // for each element in array, take the number and add to the total then return
+    param1.forEach((num) => {
+        total += num;
+    });
+    return total;
 }
-console.log(add(1, 2));
-console.log(add(3, 2));
+// console.log(add(1, 2));
+// console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
 
